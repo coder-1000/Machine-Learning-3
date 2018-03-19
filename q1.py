8d9a9dee6393376be808adab675fda90c7218d2f
De Almeida,  Paliharalalage <sahandealmeida@gmail.com>
2018-03-19 17:50:04 -0400

    made function def for q1. look at resources

---
+++ q1.py
@@ -0,0 +1,13 @@
+
+#initialize weight matrix and bias
+#then return weight sum of inputs ( wx + b)??
+def foo(x numHidden):
+    #bias = 0
+    #weight matrix = xavier initialized
+    #https://www.tensorflow.org/api_docs/python/tf/get_variable
+    #https://www.tensorflow.org/api_docs/python/tf/contrib/layers/xavier_initializer    
+    
+    #stuff that might be useful
+
+    #https://stackoverflow.com/questions/47167409/using-weights-initializer-with-tf-nn-conv2di
+    #https://github.com/aymericdamien/TensorFlow-Examples/blob/master/examples/3_NeuralNetworks/convolutional_network_raw.py
