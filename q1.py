8e081deb95fa9d9d36d357cb0fd808922bd9b355
De Almeida,  Paliharalalage <sahandealmeida@gmail.com>
2018-03-19 17:53:19 -0400

    test

--- q1.py
+++ q1.py
@@ -1,3 +1,16 @@
+#question 1
+'''
+1. layer-wise building block: Write a vectorized Tensorflow Python function that takes the
+2
+1.2 Effect of hyperparameters [5 pt.] 1 NEURAL NETWORKS [25 PT.]
+hidden activations from the previous layer then return the weighted sum of the inputs(i.e. the
+z) for the current hidden layer. You will also initialize the weight matrix and the biases in
+the same function. You should use Xavier initialization for the weight matrix. Your function
+should be able to compute the weighted sum for all the data points in your mini-batch at
+once using matrix multiplication. It should not contain loops over the training examples in
+the mini-batch. The function should accept two arguments, the input tensor and the number
+of the hidden units. Include the snippets of the Python code. [3 pt.]
+'''
 
 #initialize weight matrix and bias
 #then return weight sum of inputs ( wx + b)??
