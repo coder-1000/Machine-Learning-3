80888b7c48041300ba5cffceacb973e30698ebb8
De Almeida,  Paliharalalage <sahandealmeida@gmail.com>
2018-04-01 23:28:51 -0400

    finished all of 1.1

--- relu_activation.py
+++ relu_activation.py
@@ -6,9 +6,10 @@ import matplotlib.pyplot as plt
 NUM_HIDDEN_UNITS = 1000;
 NUM_UNITS_OUTPUT_LAYER = 10;
 BATCH_SIZE = 500;
-NUM_ITERATIONS = 5000;
+NUM_ITERATIONS = 505050505000;
 NUM_PIXELS = 784
 LAMDA = 0.0003
+LEARNING_RATES = [0.005, 0.001, 0.0001]
 
 if __name__ == "__main__":
 
@@ -59,166 +60,119 @@ if __name__ == "__main__":
     misclassification = tf.reduce_mean( tf.cast(tf.not_equal(tf.argmax(s2,axis=1), y), tf.float64) )
     #misclassification = tf.argmax(s2,axis=1);
 
-    learningRate = 0.001
     
-    descendGradient = tf.train.AdamOptimizer(learningRate).minimize(loss);
-    
-    initializer = tf.global_variables_initializer();
    
     #store cross entropy error
-    crossTrainVals = []
-    crossValidVals = []
-    crossTestVals = []
+    crossTrainVals = [[],[],[]]
+    crossValidVals = [[],[],[]] 
+    crossTestVals = [[],[],[]]
     
     #store minimum value to detect early stopping
-    minCrossTrain = 9999
-    minCrossValid = 9999
-    minCrossTest = 9999
+    minCrossTrain = [9999, 9999, 9999]
+    minCrossValid = [9999, 9999, 9999]
+    minCrossTest = [9999, 9999, 9999]
 
     #store classifcation error
-    classTrainVals = []
-    classValidVals = []
-    classTestVals = [] 
+    classTrainVals = [[],[],[]]
+    classValidVals = [[],[],[]]
+    classTestVals = [[],[],[]] 
     
     #store min value for early stopping
-    minClassTrain = 9999
-    minClassValid = 9999
-    minClassTest = 9999
+    minClassTrain = [9999, 9999, 9999]
+    minClassValid = [9999, 9999, 9999]
+    minClassTest = [9999, 9999, 9999]
 
     with tf.Session() as sess:
         
-        sess.run(initializer)
         start = 0;
        
         num_batches = trainX.shape[0] // BATCH_SIZE
         
-        for i in range(NUM_ITERATIONS):
+        for lr in range( len(LEARNING_RATES) ):
             
-            end = start + BATCH_SIZE;
-            sess.run(descendGradient, feed_dict={x0:  trainX[start:end], y: trainY[start: end] })
-            if( ((i+1)% num_batches) == 0):
-                print(i)
-
-                #get cross entropy loss values
-                trainCross = sess.run(loss, feed_dict={x0:  trainX, y: trainY })
-                validCross = sess.run(loss, feed_dict={x0: validX , y: validY })
-                testCross = sess.run(loss, feed_dict={x0: testX , y: testY })              
-                
-                minCrossTrain = min(minCrossTrain, trainCross)
-                minCrossValid = min(minCrossValid, validCross)
-                minCrossTest = min(minCrossTest, testCross)
-
-                crossTrainVals.append(trainCross)
-                crossValidVals.append(validCross)
-                crossTestVals.append(testCross)
-                
-                #get classification error values
-                trainClass = sess.run(misclassification, feed_dict ={x0: trainX, y:  trainY})
-                validClass = sess.run(misclassification, feed_dict ={x0: validX, y:  validY})
-                testClass = sess.run(misclassification, feed_dict ={x0: testX, y:  testY})
-
-                minClassTrain = min(minClassTrain, trainClass)
-                minClassValid = min(minClassValid, validClass)
-                minClassTest = min(minClassTest, testClass)
-
-                classTrainVals.append(trainClass)
-                classValidVals.append(validClass)
-                classTestVals.append(testClass)
+            learningRate = LEARNING_RATES[lr]
+            descendGradient = tf.train.AdamOptimizer(learningRate).minimize(loss);
             
-            #increment batch
-            start = end % numTrainingPoints
-        
+            #initializer has to be done after declaring adam optimizer
+            initializer = tf.global_variables_initializer();
+            
+            sess.run(initializer)
+            
+            for i in range(NUM_ITERATIONS): 
+            
+                end = start + BATCH_SIZE;
+                sess.run(descendGradient, feed_dict={x0:  trainX[start:end], y: trainY[start: end] })
+                
+                if( ((i+1)% num_batches) == 0):
+                    print(i)
+
+                    #get cross entropy loss values
+                    trainCross = sess.run(loss, feed_dict={x0:  trainX, y: trainY })
+                    validCross = sess.run(loss, feed_dict={x0: validX , y: validY })
+                    testCross = sess.run(loss, feed_dict={x0: testX , y: testY })              
+                    
+                    minCrossTrain[lr]= min(minCrossTrain[lr], trainCross)
+                    minCrossValid[lr] = min(minCrossValid[lr], validCross)
+                    minCrossTest[lr] = min(minCrossTest[lr], testCross)
+
+                    crossTrainVals[lr].append(trainCross)
+                    crossValidVals[lr].append(validCross)
+                    crossTestVals[lr].append(testCross)
+                    
+                    #get classification error values
+                    trainClass = sess.run(misclassification, feed_dict ={x0: trainX, y:  trainY})
+                    validClass = sess.run(misclassification, feed_dict ={x0: validX, y:  validY})
+                    testClass = sess.run(misclassification, feed_dict ={x0: testX, y:  testY})
+
+                    minClassTrain[lr] = min(minClassTrain[lr], trainClass)
+                    minClassValid[lr] = min(minClassValid[lr], validClass)
+                    minClassTest[lr] = min(minClassTest[lr], testClass)
+
+                    classTrainVals[lr].append(trainClass)
+                    classValidVals[lr].append(validClass)
+                    classTestVals[lr].append(testClass)
+                
+                #increment batch
+                start = end % numTrainingPoints
+            
         #get list of numbers from 0 to num iterations
         xVals = np.arange(len(crossTrainVals));
-
-        print("minimum cross Train was " + str(minCrossTrain))
-        print("minimum cross Valid  was " + str(minCrossValid))
-        print("minimum cross Test was " + str(minCrossTest))
-
-        print("minimum class Train was " + str(minClassTrain))
-        print("minimum class Train was " + str(minClassTrain))
-        print("minimum class Train was " + str(minClassTrain))
-
-
-
-
-
-        plt.plot(xVals, crossTrainVals, label= "training pts" )
-        plt.plot(xVals, crossValidVals, label= "validation pts" )
-        plt.plot(xVals, crossTestVals, label="test points")
-
-        plt.xlabel('Epoch #');
-        plt.ylabel('Loss');
-        plt.legend();
-        plt.title("Cross Entropy Loss vs Num Epochs")
-
-        plt.show() 
-    
-        plt.figure()
-
-        plt.plot(xVals, classTrainVals, label="training pts" )
-        plt.plot(xVals, classValidVals, label="validation pts" )
-        plt.plot(xVals, classTestVals, label="test points")
-
-        plt.xlabel('Epoch #');
-        plt.ylabel('Classification Error');
-        plt.legend();
-        plt.title("Classification Error vs Num Epochs")
         
-        plt.show()
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    #misclassifications = []
-    #learning_rate = [0.001, 0.005, 0.0001]
-        
+        for lr in range(len(LEARNING_RATES)):
 
-    #for lR in learning_rate:
-        # we now need a loss function of some kind
-        #xLayer0 = inputs; #our initial inputs
-        #the weighted sum matrix should return a 1000x1 vector 
-        #sLayer1 = weighted_matrix(inputs, NUM_HIDDEN_UNITS) #our hidden layer
-        #our x output layer should also be 1000x1
-        #xLayer1 = tf.nn.relu(sLayer1); #technically our output layer
-        #this should output a 1x10 vector with the 10 classes
-        #outputLayer = weighted_matrix(xLayer1, NUM_HIDDEN_UNITS_OUTPUT_LAYER);
-        
-        #add some weight decay to prevent overfitting
-        #
+            print("learning rate  = " + str(LEARNING_RATES[lr]))
 
+            print("minimum cross Train was " + str(minCrossTrain[lr]))
+            print("minimum cross Valid  was " + str(minCrossValid[lr]))
+            print("minimum cross Test was " + str(minCrossTest[lr]))
+
+            print("minimum class Train was " + str(minClassTrain[lr]))
+            print("minimum class Valid was " + str(minClassTrain[lr]))
+            print("minimum class Test was " + str(minClassTrain[lr]))
+
+            plt.plot(xVals, crossTrainVals[lr], label= "training pts" )        
+            plt.plot(xVals, crossValidVals[lr], label= "validation pts" )
+            plt.plot(xVals, crossTestVals[lr], label="test points")
+
+            plt.xlabel('Epoch #');
+            plt.ylabel('Loss');
+            plt.legend();
+            plt.title("Cross Entropy Loss vs Num Epochs with LR = " + str(LEARNING_RATES[lr]) )
+
+            plt.show() 
+        
+            plt.figure()
+
+            plt.plot(xVals, classTrainVals[lr], label="training pts" )
+            plt.plot(xVals, classValidVals[lr], label="validation pts" )
+            plt.plot(xVals, classTestVals[lr], label="test points")
+
+            plt.xlabel('Epoch #');
+            plt.ylabel('Classification Error');
+            plt.legend();
+            plt.title("Classification Error vs Num Epochs with LR = " + str(LEARNING_RATES[lr]))
+            
+            plt.show()
     
-    #misclassifications = []
-    #learning_rate = [0.001, 0.005, 0.0001]
-        
+   
 
-    #for lR in learning_rate:
-        # we now need a loss function of some kind
-        #xLayer0 = inputs; #our initial inputs
-        #the weighted sum matrix should return a 1000x1 vector 
-        #sLayer1 = weighted_matrix(inputs, NUM_HIDDEN_UNITS) #our hidden layer
-        #our x output layer should also be 1000x1
-        #xLayer1 = tf.nn.relu(sLayer1); #technically our output layer
-        #this should output a 1x10 vector with the 10 classes
-        #outputLayer = weighted_matrix(xLayer1, NUM_HIDDEN_UNITS_OUTPUT_LAYER);
-        
-        #add some weight decay to prevent overfitting
-        #loss = tf.nn.softmax_cross_entropy_with_logits(logits = outputLayer, label = actualLabel);
-	#trainStep = tf.train.GradientOptimizer(lR).minimize(loss);
-
-	#misclassfications, add to the misclassifications list if they are not equal, therfore misclassified
-	#missclassifications.append(not tf.equal(tf.transpose(outputLayer), actualLabel))
